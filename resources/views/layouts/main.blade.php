7850efa9541e618501b0bacf69ea870992f3d54e
stunggal <54833890+stunggal@users.noreply.github.com>
2022-08-11 21:47:10 +0700

    halaman 404

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -216,10 +216,6 @@
             &copy; Copyright <strong><span>Tracer Study</span></strong>. All Rights Reserved
         </div>
         <div class="credits">
-            <!-- All the links in the footer should remain intact. -->
-            <!-- You can delete the links only if you purchased the pro version. -->
-            <!-- Licensing information: https://bootstrapmade.com/license/ -->
-            <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/nice-admin-bootstrap-admin-html-template/ -->
             Created by <a href="https://bootstrapmade.com/">PPTIK UNIDA Gontor</a>
         </div>
     </footer><!-- End Footer -->
